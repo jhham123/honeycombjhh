430129eb2f36dd247ec1ddddf27eb8addd42e4f7
william-schor <william_schor@brown.edu>
2021-01-17 00:38:50 -0700

    slider fixes

--- src/lib/markup/numberBoxes.js
+++ src/lib/markup/numberBoxes.js
@@ -3,31 +3,31 @@ export const numberBoxes = (left, rightTop, rightBot) => {
     return `
     <div class="grid_container" style="background-color: black">
       <div class="fixation-plus"></div>
-      <div class="left">${left}</div>
-      <div class="right-top">${rightTop}</div>
-      <div class="right-bot">${rightBot}</div>
+      <div class="box left">${left}</div>
+      <div class="box right-top">${rightTop}</div>
+      <div class="box right-bot">${rightBot}</div>
     </div>
     `;
   } else if (rightTop !== null && rightBot !== null) {
     return `
     <div class="grid_container" style="background-color: black">
       <div class="fixation-plus"></div>
-      <div class="right-top">${rightTop}</div>
-      <div class="right-bot">${rightBot}</div>
+      <div class="box right-top">${rightTop}</div>
+      <div class="box right-bot">${rightBot}</div>
     </div>
     `;
   } else if (rightTop !== null) {
     return `
     <div class="grid_container" style="background-color: black">
       <div class="fixation-plus"></div>
-      <div class="right-top">${rightTop}</div>
+      <div class="box right-top">${rightTop}</div>
     </div>
     `;
   } else if (rightBot !== null) {
     return `
     <div class="grid_container" style="background-color: black">
       <div class="fixation-plus"></div>
-      <div class="right-bot">${rightBot}</div>
+      <div class="box right-bot">${rightBot}</div>
     </div>
     `;
   }
--- src/trials/happiness_slider.js
+++ src/trials/happiness_slider.js
@@ -1,14 +1,16 @@
 import {jsPsych} from "jspsych-react";
 // "jspsych/plugins/jspsych-image-keyboard-response.js"
 
-const happiness_slider =  {
-    type: "html_slider_response",
-    stimulus: '<div class="happiness_stimulus">How happy are you at this moment</div>',
-    labels: ['very unhappy', 'very happy'],
-    require_movement: true,
-    button_label: 'Continue',
-    // trial_duration: 4000,
-    response_ends_trial: true
-}
+const happiness_slider = {
+  type: "html_slider_response",
+  stimulus:
+    '<div class="happiness_stimulus">How happy are you at this moment</div>',
+  labels: ["very unhappy", "very happy"],
+  require_movement: true,
+  button_label: "Continue",
+  // trial_duration: 4000,
+  response_ends_trial: true,
+  step: 10
+};
 
-export default happiness_slider
+export default happiness_slider;
